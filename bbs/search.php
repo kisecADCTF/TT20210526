8c73052476a73b29772a3c1715fdef608d04c7c0
tnatna03@naver.com <tnatna03@naver.com>
2021-05-28 17:52:07 +0900

    게시판 검색기능추가2

--- bbs/search.php
+++ bbs/search.php
@@ -7,18 +7,27 @@ $search = $_GET['search'];
 
 $sql= "select * from post where post_name like '%$search%' order by post_ID desc";
 $result = mysqli_query($conn, $sql) or die(mysqli_error($conn));
-if(isset($result))
+if(!empty($result)){
+    $cnt = 0;
     while ($row = mysqli_fetch_array($result))
     {
-        $post_ID = $row['post_ID'];
-        $post_name = $row['post_name'];
-        $author_ID = $row['user_ID'];
-        $post_link = "<a href='post.php?post_ID=$post_ID'>$post_name</a>";
-        echo <<< EOT
+        if(isset($row))
+        {
+            $cnt++;
+            $post_ID = $row['post_ID'];
+            $post_name = $row['post_name'];
+            $author_ID = $row['user_ID'];
+            $post_link = "<a href='post.php?post_ID=$post_ID'>$post_name</a>";
+            echo <<< EOT
             <p><h5>
                 $post_link
             </h5></p>
 EOT;
+        }
     }
-    else
-        echo "찾는 글이 없습니다.";
+    if($cnt == 0)
+    {
+        echo "찾을 수 없습니다";
+    }
+}
+
