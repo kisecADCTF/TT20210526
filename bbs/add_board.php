1d32b5b4ed9af9d92005a5592530e1b39033f4aa
tnatna03@naver.com <tnatna03@naver.com>
2021-05-28 16:38:01 +0900

    board name filter

--- bbs/add_board.php
+++ bbs/add_board.php
@@ -4,13 +4,16 @@ include('../utils/constants.php');
 include('../utils/connect.php');
 include('../utils/general.php');
 
+$NO = array("/</","/>/","/\"/");
+$YES = array("&lt","&gt","&quot;");
+
 if (!isset($_POST['submit']))
 	exit('Illegal call to this page.');
 if (!($_SESSION['default_permission'] >= PERM_ADMINISTRATOR))
 	exit('Not enough permission.');
 
 $board_name = $_POST['board_name'];
-$board_name =preg_replace("/[ #\&\+\-%@=\/\\\:;,\.'\"\^`~\_|\!\?\*$#<>()\[\]\{\}]/i","",$board_name);
+$board_name=preg_replace($NO,$YES,$board_name);
 $board_name = addslashes($board_name);
 $query = "INSERT INTO board(board_name) VALUES ('$board_name')";
 
